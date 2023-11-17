e5764140b46b691c8aa345e373edfb0433df1718
Dosimz <vhzix24@gmail.com>
2023-11-17 10:30:57 +0800

    day19

--- day19/subTreeWithAll.cc
+++ day19/subTreeWithAll.cc
@@ -13,7 +13,37 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Solution {
+public:
+    struct NodeDep{
+        TreeNode* node;
+        int depth;
 
+        NodeDep(TreeNode* n, int d){
+            node = n;
+            depth = d;
+        }
+    };
+
+    NodeDep maxDepth(TreeNode* node){
+        if(node==nullptr) return NodeDep(nullptr, 0);
+        NodeDep left = maxDepth(node->left);
+        NodeDep right = maxDepth(node->right);
+
+        if(left.depth == right.depth){
+            return NodeDep(node, left.depth+1);
+        }
+
+        auto res = left.depth >right.depth ? left : right;
+        res.depth++;
+        return res;
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        auto res = maxDepth(root);
+        return res.node;
+    }
+};
 
 class Solution {
 public:
