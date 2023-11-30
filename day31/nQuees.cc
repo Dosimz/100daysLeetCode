2a6cf3f436a36a4393dae053612ffde320622908
Dosimz <vhzix24@gmail.com>
2023-11-30 10:28:06 +0800

    day31

---
+++ day31/nQuees.cc
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        backtrack(board, 0);
+        return res;
+    }
+
+    void backtrack(vector<string> board, int row){
+        if(row==board.size()){
+            res.push_back(board);
+            return;
+        }
+
+
+        for(int col=0; col<board[row].size(); col++){
+            if(!isValid(board, row, col)){
+                continue;
+            }
+
+            board[row][col] = 'Q';
+            backtrack(board, row+1);
+            board[row][col] = '.';
+        }
+    }
+
+    bool isValid(vector<string> board, int row, int col){
+        for(int i=0; i<=row; i++){
+            if(board[i][col]=='Q') return false;
+        }
+
+        for(int i=row-1, j=col+1;i>=0&&j<board[row].size(); i--, j++){
+            if(board[i][j]=='Q') return false;
+        }
+
+        for(int i=row-1, j=col-1; i>=0&&j>=0; i--, j--){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
+    }
+};
